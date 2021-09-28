d496e525cd784e6af5841a6bd4d81f3af918387a
unknown <greshje.nachc@gmail.com>
2021-09-28 19:08:49 -0400

    working on tests

--- test/garble/basic/garble_simple_integration_test.py
+++ test/garble/basic/garble_simple_integration_test.py
@@ -5,11 +5,20 @@ from types import SimpleNamespace
 
 def main():
     print("Starting test...")
-    source_file = fu.get_file_name("test-data/data-owner/basic/hospital-001/hospital_001.csv")
-    secret_file = fu.get_file_name("test-data/data-owner/basic/hospital-001/secret-file.txt")
+    do_test("hospital_001")
+    do_test("hospital_002")
+    do_test("hospital_003")
+    print("\n\nDone.")
+
+
+def do_test(name):
+    print("-----------------------------------")
+    print("Doing test for: " + name)
+    source_file = fu.get_file_name("test-data/data-owner/basic/"+name+"/"+name+".csv")
+    secret_file = fu.get_file_name("test-data/data-owner/basic/"+name+"/secret-file.txt")
     schema_dir = fu.get_file_name("test-data/schemas/default-schema")
-    output_dir = fu.get_file_name("test-data/data-owner/basic/hospital-001/out")
-    output_zip = "hosp-001.zip"
+    output_dir = fu.get_file_name("test-data/data-owner/basic/"+name+"/out")
+    output_zip = name+".zip"
     args = {
         "sourcefile": source_file,
         "secretfile": secret_file,
@@ -21,7 +30,6 @@ def main():
     print(args)
     clks = garble.garble_pii(args)
     garble.create_clk_zip(clks, args)
-    print("Done.")
 
 
 if __name__ == "__main__":
